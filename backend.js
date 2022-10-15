f65448587ca0fa58b7b834fe08730866570eccde
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-10-15 20:03:03 +0000

    Console logging start, end, and days

--- backend.js
+++ backend.js
@@ -17,19 +17,28 @@ function readFile(input) {
 
 function fileToArray(icsInfo) {
   var icsArray = icsInfo.split("\n");
-  console.log(icsArray[0]);
+  //console.log(icsArray[0]);
   var course = 0;
   var timing = [];
   for (var i in icsArray) {
     if (icsArray[i].includes("DTSTART;TZID")) {
+      icsArray[i] = icsArray[i].substring(icsArray[i].lastIndexOf("T") + 1,icsArray[i].length);
       timing[course] = icsArray[i];
       course++;
       console.log(icsArray[i]);
     }
     if (icsArray[i].includes("DTEND;TZID")) {
+      icsArray[i] = icsArray[i].substring(icsArray[i].lastIndexOf("T") + 1,icsArray[i].length);
+      timing[course] = icsArray[i];
+      course++;
+      console.log(icsArray[i]);
+    }
+    if (icsArray[i].includes("RRULE:FREQ=WEEKLY")) {
+      icsArray[i] = icsArray[i].substring(icsArray[i].lastIndexOf("=") + 1,icsArray[i].length);
       timing[course] = icsArray[i];
       course++;
       console.log(icsArray[i]);
     }
   }
 }
+

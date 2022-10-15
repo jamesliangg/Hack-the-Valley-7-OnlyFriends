8d291b1fcb9a7d8c183508850b7656e3050ad074
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-10-15 19:04:25 +0000

    Updated JS

--- backend.js
+++ backend.js
@@ -5,27 +5,31 @@ function functionName(s) {
 
 
 function readFile(input) {
+  var input = document.getElementById("userInput").value;
+  alert(input);
+
+
   let file = input.files[0];
   let fileReader = new FileReader();
   fileReader.readAsText(file);
   fileReader.onload = function() {
-    alert(fileReader.result);
+    //var fileStuff = fileReader.result;
+    //return fileStuff;
+    console.log(fileReader.results);
   };
   fileReader.onerror = function() {
     alert(fileReader.error);
   };
 }
 
-
-function read() {
-  alert("test");
-  const fs = require('fs')
-  let fInput = "You are reading the content from Tutorials Point"
-  fs.writeFile('tp.txt', fInput, (err) => {
-    if (err) throw err;
-    else {
-      alert("The file is updated with the given data");
-    }
-  })
-
+async function read(file) {
+  // Read the file as text
+  console.log(await file.text())
+  // Read the file as ArrayBuffer to handle binary data
+  console.log(new Uint8Array(await file.arrayBuffer()))
+  // Abuse response to read json data
+  console.log(await new Response(file).json())
+  // Read large data chunk by chunk
+  console.log(file.stream())
 }
+

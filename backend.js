3c156daad231e0dad6f5ebe2217ab748ae40fb67
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-10-15 19:21:33 +0000

    Working HTML

--- backend.js
+++ backend.js
@@ -1,35 +1,31 @@
 // document.body.innerHTML = '<h1>This is JS</h1>';
-function functionName(s) {
-  alert('Hello, ' + s + '!');
+function functionName() {
+  alert(`Hello!`);
 }
 
+//  var input = document.getElementById("userInput").value;
+//  alert(input);
 
 function readFile(input) {
-  var input = document.getElementById("userInput").value;
-  alert(input);
-
-
   let file = input.files[0];
   let fileReader = new FileReader();
   fileReader.readAsText(file);
-  fileReader.onload = function() {
+  fileReader.onload = function(){
     alert(fileReader.result);
-    //return fileStuff;
-
+    // return fileStuff;
   };
   fileReader.onerror = function() {
     alert(fileReader.error);
   };
 }
 
-async function read(file) {
-  // Read the file as text
-  console.log(await file.text())
-  // Read the file as ArrayBuffer to handle binary data
-  console.log(new Uint8Array(await file.arrayBuffer()))
-  // Abuse response to read json data
-  console.log(await new Response(file).json())
-  // Read large data chunk by chunk
-  console.log(file.stream())
-}
-
+// async function read(file) {
+//   // Read the file as text
+//   console.log(await file.text())
+//   // Read the file as ArrayBuffer to handle binary data
+//   console.log(new Uint8Array(await file.arrayBuffer()))
+//   // Abuse response to read json data
+//   console.log(await new Response(file).json())
+//   // Read large data chunk by chunk
+//   console.log(file.stream())
+// }

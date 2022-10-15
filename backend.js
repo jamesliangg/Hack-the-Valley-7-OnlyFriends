1cb6312969c3a36d443838106a23790a2a1720f8
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-10-15 16:58:56 +0000

    Removed Python

--- backend.js
+++ backend.js
@@ -1,5 +1,31 @@
 // document.body.innerHTML = '<h1>This is JS</h1>';
+function functionName(s) {
+  alert('Hello, ' + s + '!');
+}
 
-function functionName( s ){
-	alert('Hello, ' + s + '!');
+
+function readFile(input) {
+  let file = input.files[0];
+  let fileReader = new FileReader();
+  fileReader.readAsText(file);
+  fileReader.onload = function() {
+    alert(fileReader.result);
+  };
+  fileReader.onerror = function() {
+    alert(fileReader.error);
+  };
+}
+
+
+function read() {
+  alert("test");
+  const fs = require('fs')
+  let fInput = "You are reading the content from Tutorials Point"
+  fs.writeFile('tp.txt', fInput, (err) => {
+    if (err) throw err;
+    else {
+      alert("The file is updated with the given data");
+    }
+  })
+
 }
